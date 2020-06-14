741a1474623207f94f75b17b9fad475876ffaedf
Jonathan Nocjar <no-reply>
2020-06-14 14:04:45 -0400

    Some gitignore changes. Support for "ant run".

--- src/Sim.java
+++ src/Sim.java
@@ -3,7 +3,7 @@ public class Sim
 {
     public Sim() {}
 
-    public static void main(String[] args)    
+    static public void main(String[] args)    
     {
         System.out.println("Welcome to Sim built by Ant");
     }
